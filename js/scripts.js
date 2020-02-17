6a49a18361b762894658787368b04bfae79a05bd
Andrew Kim <andrewswkim47@gmail.com>
2020-02-16 20:54:10 -0800

    update backend and frontend logic. function not defined still.

--- js/scripts.js
+++ js/scripts.js
@@ -2,80 +2,45 @@
   // Business Logic for Tickets ---------
   // This is a constructor
   function Ticket(movie, time, age) {
-    this.movie = movie,
-    this.time = time,
-    this.age = age
+    this.movie = movie;
+    this.time = time;
+    this.age = age;
   }
   
   // This is a prototype method to the constructor
-  Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName;
-  }
   
-  // User Interface Logic ---------
-  // The first function shows contacts as they're being added to the address book (first and last name is visible)
-  // For each contact, assign the variable htmlForContactInfo an html list element with an id, first name and last name. This Li will go into the empty ul element.
-  var addressBook = new AddressBook();
-  // here we created a new AddressBook object as a global variable
-  
-  function displayMovieDetails(MoviesToDisplay) {
-    var contactsList = $("ul#contacts");
-    var htmlForContactInfo = "";
-    addressBookToDisplay.contacts.forEach(function(contact) {
-      htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + " " + contact.emailAddress + " " + contact.workEmailAddress + " " + contact.physicalAddress + "</li>";
-    });
-    contactsList.html(htmlForContactInfo);
-  };
-  // The second function is for the event listener to show/hide the div with visible first and last name and phone number.
-  function showContact(contactId) {
-    var contact = addressBook.findContact(contactId);
-    $("#show-contact").show();
-    $(".first-name").html(contact.firstName);
-    $(".last-name").html(contact.lastName);
-    $(".phone-number").html(contact.phoneNumber);
-    $(".email-address").html(contact.emailAddress);
-    $(".work-email-address").html(contact.workEmailAddress);
-    $(".physical-address").html(contact.physicalAddress);
-    var buttons = $("#buttons");
-    buttons.empty();
-    buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
-  }
-  // The third function - the on method within this function takes two arguments, 1) click on parent ul element 2) click on child li element
-  // the click on the parent ul element is delegated to its child li elements, or the li click event bubbles up to the parent.
-  // after submitting the form this will show first and last,when you click the li element you get first and last name and phone number returned.
-  // the parent element 
-  function attachTicketPrices() {
-    $("ul#contacts").on("click", "li", function() {
-     showContact(this.id);
-         //show contact method passes this.id as an argument and returns an id based on the findContact prototype method
-    });
+  Ticket.prototype.showPrice = function() {
+    var price = 0;
+    
+    if (this.movie.includes('Uncut Gems')) {
+        price += 13;
+        console.log(this.movie);
+    };
+    if (this.time.includes('7:30 pm')) {
+        price += 15;
+    } else {
+      price += 14;
+    };
+    if (this.age.includes('55+')) {
+      price += 8;
+    } else if (this.age.includes('19-22')) {
+      price += 11;
+    } else {
+      price += 12;
+    }
+    this.price = price;
   };
 
 
   $(document).ready(function() {
-      
     $("form#price-show").submit(function(event) {
       event.preventDefault();
-
-    
- 
-    //   var inputtedFirstName = $(".dropdown-item").val();
-    //   var inputtedLastName = $("input#new-last-name").val();
-    //   var inputtedPhoneNumber = $("input#new-phone-number").val();
-    //   var inputtedEmailAddress = $("input#new-email-address").val();
-    //   var inputtedWorkEmailAddress = $("input#new-work-email-address").val();
-    //   var inputtedPhysicalAddress = $("input#new-physical-address").val();
-    //   $("input#new-first-name").val("");
-    //   $("input#new-last-name").val("");
-    //   $("input#new-phone-number").val("");
-    //   $("input#new-email-address").val("");
-    //   $("input#new-work-email-address").val("");
-    //   $("input#new-physical-address").val("");
-    //   $("p").not(".not-this").remove();
-    
-
-    //   var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedWorkEmailAddress, inputtedPhysicalAddress);
-    //   addressBook.addContact(newContact);
-    //   displayContactDetails(addressBook);
+      var movie = $("#movies").val("");
+      var time = $("#times").val("");
+      var age = $("#age").val("");
+      var ticketResults = new Ticket(movie, time, age);
+      ticketResults.showPrice();
+      console.log(ticketResults);
+      $("#price-results").text("Thank you! Your price for " + ticketResults.movie + " was " + ticketResults.price + " at " + ticketResults.time + "pm. Your price was dependendent on your " + ticketResults.age + " age(range) ");
     })
   })
